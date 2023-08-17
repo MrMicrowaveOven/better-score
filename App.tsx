377d09b72e90ecb63746dac0f109dbe8efe4c9c8
mrmicrowaveoven <benj@zagorski.com>
2023-08-17 16:10:47 -0500

    More files for PagerView

--- App.tsx
+++ App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
 import ScoreBoard from './public/ScoreBoard';
 import LineUp from './public/LineUp'
+import PagerView from 'react-native-pager-view';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <ScoreBoard />
-      {/* <LineUp /> */}
-    </SafeAreaView>
+    <PagerView style={styles.pagerView} initialPage={0}>
+      <View key="1">
+        <ScoreBoard />
+      </View>
+      <View key="2">
+        <LineUp />
+      </View>
+    </PagerView>
   )
 }
+
+const styles = StyleSheet.create({
+  pagerView: {
+    flex: 1,
+  },
+});
 
 export default App;
