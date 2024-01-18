1b61ab58789d5277e592fcc317b4ddbd5eab4328
mrmicrowaveoven <benj@zagorski.com>
2024-01-18 01:18:19 -0600

    Added Checkbox component

--- public/components/SettingsWindow.tsx
+++ public/components/SettingsWindow.tsx
@@ -76,6 +76,7 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         justifyContent: "flex-end",
         alignItems: "center",
+        marginRight: 10,
     },
     optionCheckbox: {
         right: 10,
